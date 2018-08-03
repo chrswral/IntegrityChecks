2665508276d7884b5202a1f1c567c4e205cc4548
Carl Black <carl.black@russelladams.com>
2018-08-03 16:38:09 +0100

    Updated General Integrity Fixes to include all original fixes with updated audit history

--- GeneralIntegrityFixesWithAuditHistory.sql
+++ GeneralIntegrityFixesWithAuditHistory.sql
@@ -2,14 +2,15 @@ SET XACT_ABORT ON
 
 BEGIN TRAN
 
-/*Remove orphaned links to missing sDemandPart */
-
+/* Create Pending Table */
 DECLARE @AuditHistoryPending TABLE (
     BaseTableID INT NOT NULL,
     Fix VARCHAR(50) NOT NULL,
     BaseTable VARCHAR(50)
 )
 
+
+/*Remove orphaned links to missing sDemandPart */
 
 UPDATE sStock
 SET sStockOwnership_ID = sOrderPartReceipt.sStockOwnership_ID
@@ -19,10 +20,53 @@ FROM sStock
 JOIN sOrderPartReceipt ON sOrderPartReceipt.ID = sStock.sOrderPartReceipt_ID
 WHERE sStock.sStockOwnership_ID = 0
 
+/* Fix missing stock locations */
+UPDATE sStock
+SET sBaseWarehouseLocation_ID = 
+ISNULL(
+	(SELECT TOP 1 sBaseWarehouseLocation_ID FROM sStockLog WHERE BaseTableID = sStock.ID  AND sBaseWarehouseLocation_ID > 0 ORDER BY ID DESC) ,  -- Try and get location from sStockLog first
+	(SELECT TOP 1 sBaseWarehouseLocation_ID FROM sOrderPartReceipt WHERE ID = sStock.sOrderPartReceipt_ID ORDER BY ID DESC))	  -- Otherwise, get from receipt
+OUTPUT deleted.ID,'Missing Location','sStock'
+INTO @AuditHistoryPending    
+FROM sStock 
+LEFT JOIN sBaseWarehouseLocation ON sStock.sBaseWarehouseLocation_ID = sBaseWarehouseLocation.ID 
+WHERE sBaseWarehouseLocation.ID IS NULL
+
+
+/*Remove FK to completed sDemandPart */
+UPDATE sStock
+SET sDemandPart_ID = 0
+OUTPUT deleted.ID,'Remove FK to completed sDemandPart','sStock'
+INTO @AuditHistoryPending
+FROM sStock
+JOIN sDemandPart ON sStock.sDemandPart_ID = sDemandPart.ID
+JOIN sDemandItemStatus ON sDemandItemStatus.ID = sDemandPart.sDemandItemStatus_ID
+JOIN sPartTransactionType ON sPartTransactionType.ID = sPartTransactionType_ID
+WHERE sDemandItemStatus.Completed = 1 AND sPartTransactionType.Replenishment = 1
+
+/*Remove FK to Issued sDemandPart */
+UPDATE sStock
+SET sDemandPart_ID = 0
+OUTPUT deleted.ID,'Remove FK to Issued sDemandPart','sStock'
+INTO @AuditHistoryPending
+FROM sStock
+JOIN sDemandPart ON sStock.sDemandPart_ID = sDemandPart.ID
+JOIN sDemandItemStatus ON sDemandItemStatus.ID = sDemandPart.sDemandItemStatus_ID
+WHERE sDemandItemStatus.Issued = 1
+
+
+/*Remove orphaned links to missing sDemandPart */
+UPDATE sStock
+SET sDemandPart_ID = 0
+OUTPUT deleted.ID,'Remove orphaned links to missing sDemandPart','sStock'
+FROM sStock
+LEFT JOIN sDemandPart ON sStock.sDemandPart_ID = sDemandPart.ID
+WHERE sDemandPart.ID IS NULL AND sStock.sDemandPart_ID > 0
+
+
 INSERT INTO sup.AuditHistory(BaseTable,BaseTableID,Fix)
 SELECT BaseTable,BaseTableID,Fix FROM @AuditHistoryPending
 
-SELECT * FROM @AuditHistoryPending
 SELECT * FROM sup.AuditHistory
 
 
