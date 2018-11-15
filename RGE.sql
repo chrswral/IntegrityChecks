55d1db71b3d753c9dbd1d34ae823bc4b32d3cacc
chriswral <chris.williams@rusada.com>
2018-11-15 14:38:16 +0000

    Fixed RGE StockInteg

--- RGE.sql
+++ RGE.sql
@@ -46,3 +46,16 @@ WHERE sDemandItemStatus.Cancelled = 0
 ORDER BY sStock.ID
 OFFSET 40 ROWS FETCH NEXT 5 ROWS ONLY)
 
+DELETE FROM sStock
+WHERE sOrderPartReceipt_ID IN (
+		  SELECT sOrderPartReceipt_ID
+		  FROM sStock 
+		  JOIN sOrderPartReceipt ON sOrderPartReceipt.ID = sOrderPartReceipt_ID
+		  JOIN sPart ON sPart.ID = sOrderPartReceipt.sPart_ID
+		  JOIN sPartClassification ON sPartClassification.ID = sPartClassification_ID
+		  WHERE sStock.sDemandPart_ID = 0 
+		  AND sPartClassification.Tool = 0
+		  GROUP BY sOrderPartReceipt_ID, sOrderPartReceipt.Qty
+		  HAVING SUM(sStock.Qty) = sOrderPartReceipt.Qty
+		  ORDER BY sOrderPartReceipt_ID
+		  OFFSET 50 ROWS FETCH NEXT 1 ROWS ONLY)
