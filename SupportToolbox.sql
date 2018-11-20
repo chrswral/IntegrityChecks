532707b701a09796fa98679e529223a36d26e7f7
chriswral <chris.williams@rusada.com>
2018-11-20 10:10:42 +0000

    Strted adding useful views for diagnosing locks

--- SupportToolbox.sql
+++ SupportToolbox.sql
@@ -1112,6 +1112,46 @@ EXEC sys.sp_addextendedproperty
      @level1type = N'VIEW',
      @level1name = N'vTransaction';
 
+
+GO
+
+IF EXISTS (SELECT * FROM sys.views WHERE name = 'Locked')
+	DROP VIEW sup.Locked;
+GO
+
+CREATE VIEW sup.Locked
+AS
+SELECT dm_tran_locks.request_session_id,
+       dm_tran_locks.resource_database_id,
+       DB_NAME(dm_tran_locks.resource_database_id) AS dbname,
+       CASE
+           WHEN resource_type = 'OBJECT'
+               THEN OBJECT_NAME(dm_tran_locks.resource_associated_entity_id)
+           ELSE OBJECT_NAME(partitions.object_id)
+       END AS ObjectName,
+       partitions.index_id,
+       indexes.name AS index_name,
+       dm_tran_locks.resource_type,
+       dm_tran_locks.resource_description,
+       dm_tran_locks.resource_associated_entity_id,
+       dm_tran_locks.request_mode,
+       dm_tran_locks.request_status
+FROM sys.dm_tran_locks
+LEFT JOIN sys.partitions ON partitions.hobt_id = dm_tran_locks.resource_associated_entity_id
+LEFT JOIN sys.indexes ON indexes.object_id = partitions.object_id AND indexes.index_id = partitions.index_id
+WHERE resource_associated_entity_id > 0
+  AND resource_database_id = DB_ID()
+
+GO
+
+EXEC sys.sp_addextendedproperty
+     @name = N'HelpText',
+     @value = N'Gets locked objects',
+     @level0type = N'SCHEMA',
+     @level0name = N'sup',
+     @level1type = N'VIEW',
+     @level1name = N'Locked';
+
 GO  
 
 
