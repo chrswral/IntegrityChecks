aea6965929a784a53f30541563aada011d1e434b
Christopher Williams <chrisw@russelladams.com>
2020-03-10 11:51:20 +0000

    Updated sup.Locked with lockres query

--- Envision/SupportToolbox.sql
+++ Envision/SupportToolbox.sql
@@ -1373,21 +1373,27 @@ GO
 
 CREATE VIEW sup.Locked
 AS
-SELECT dm_tran_locks.request_session_id,
-       dm_tran_locks.resource_database_id,
+SELECT dm_tran_locks.request_session_id AS SPID,
+       --dm_tran_locks.resource_database_id,
        DB_NAME(dm_tran_locks.resource_database_id) AS dbname,
        CASE
            WHEN resource_type = 'OBJECT'
                THEN OBJECT_NAME(dm_tran_locks.resource_associated_entity_id)
            ELSE OBJECT_NAME(partitions.object_id)
        END AS ObjectName,
-       partitions.index_id,
+       --partitions.index_id,
        indexes.name AS index_name,
        dm_tran_locks.resource_type,
        dm_tran_locks.resource_description,
        dm_tran_locks.resource_associated_entity_id,
        dm_tran_locks.request_mode,
-       dm_tran_locks.request_status
+       dm_tran_locks.request_status,
+	   IIF(dm_tran_locks.resource_type = 'KEY', CONCAT('SELECT * FROM ', CASE
+           WHEN resource_type = 'OBJECT'
+               THEN OBJECT_NAME(dm_tran_locks.resource_associated_entity_id)
+           ELSE OBJECT_NAME(partitions.object_id)
+       END,' WITH (NOLOCK) WHERE %%lockres%% = ''',RTRIM(dm_tran_locks.resource_description),'''')
+	   , '') AS Query
 FROM sys.dm_tran_locks
 LEFT JOIN sys.partitions ON partitions.hobt_id = dm_tran_locks.resource_associated_entity_id
 LEFT JOIN sys.indexes ON indexes.object_id = partitions.object_id AND indexes.index_id = partitions.index_id
